857589b7d9a52014881f75757126655b72a3af19
lgdelacruz <bboyjacks@gmail.com>
2019-11-13 21:01:50 -0800

    Complete

--- src/App.js
+++ src/App.js
@@ -1,110 +1,31 @@
 import React from "react";
 import "./App.css";
-import BaseText from "./Text/ basetext";
-import Border from "./Text/border";
-import {
-  handleMouseDown,
-  handleMouseMove,
-  handleMouseUp
-} from "./Text/eventHandler";
-import TextControls from "./Text/textcontrols";
+import Text from "./Text/text";
 
 function App() {
-  const [edit, setEdit] = React.useState(false);
-  const [textData, setTextData] = React.useState(() => {
-    return {
-      id: "unique-id-123",
-      x: 50,
-      y: 60,
-      fontSize: 20,
-      fontFamily: "sans-serif",
-      fontWeight: "bold",
-      fontStyle: "italic",
-      textDecoration: "underline",
-      textAlign: "center",
-      text: "Hello World",
-      event: {
-        x: 0,
-        y: 0,
-        originalX: 0,
-        originalY: 0,
-        status: "mouse-up"
-      }
-    };
-  });
-
-  const textRef = React.useRef();
-  const borderRef = React.useRef();
-
-  React.useEffect(() => {
-    const onMouseDown = e => {
-      if (e.target === borderRef.current) {
-        setTextData(s => handleMouseDown(s, e));
-      }
-    };
-    const onMouseMove = e => {
-      setTextData(s => handleMouseMove(s, e));
-    };
-    const onMouseUp = e => {
-      setTextData(s => handleMouseUp(s, e));
-    };
-
-    const onClick = e => {
-      setEdit(s => e.target.classList.contains(textData.id));
-    };
-
-    document.addEventListener("mousedown", onMouseDown);
-    document.addEventListener("mousemove", onMouseMove);
-    document.addEventListener("mouseup", onMouseUp);
-    document.addEventListener("click", onClick);
-    return () => {
-      document.removeEventListener("click", onClick);
-      document.removeEventListener("mousedown", onMouseDown);
-      document.removeEventListener("mousemove", onMouseMove);
-      document.removeEventListener("mouseup", onMouseUp);
-    };
-  }, [textData.id]);
+  const textData = {
+    id: "unique-id-123",
+    x: 50,
+    y: 60,
+    fontSize: 20,
+    fontFamily: "sans-serif",
+    fontWeight: "bold",
+    fontStyle: "italic",
+    textDecoration: "underline",
+    textAlign: "center",
+    text: "Hello World",
+    event: {
+      x: 0,
+      y: 0,
+      originalX: 0,
+      originalY: 0,
+      status: "mouse-up"
+    }
+  };
 
   return (
     <div className="App">
-      <TextControls
-        onBoldClick={() => {
-          setTextData({
-            ...textData,
-            fontWeight: textData.fontWeight === "bold" ? "normal" : "bold"
-          });
-        }}
-        onItalicClick={() => {
-          setTextData({
-            ...textData,
-            fontStyle: textData.fontStyle === "italic" ? "normal" : "italic"
-          });
-        }}
-        onUnderlineClick={() => {
-          setTextData({
-            ...textData,
-            textDecoration:
-              textData.textDecoration === "underline" ? "none" : "underline"
-          });
-        }}
-        onFontSizeSelect={e => {
-          setTextData({
-            ...textData,
-            fontSize: parseInt(e.target.value)
-          });
-        }}
-        textData={textData}
-        edit={edit}
-      />
-      <Border edit={edit} ref={borderRef} textData={textData} color="lightgrey">
-        <BaseText
-          ref={textRef}
-          textData={textData}
-          edit={edit}
-          onClick={() => setEdit(true)}
-          onChange={e => setTextData({ ...textData, text: e.target.value })}
-        />
-      </Border>
+      <Text textData={textData} />
     </div>
   );
 }
---
+++ src/Text/text.js
@@ -0,0 +1,91 @@
+import React from "react";
+import BaseText from "./ basetext";
+import Border from "./border";
+import {
+  handleMouseDown,
+  handleMouseMove,
+  handleMouseUp
+} from "./eventHandler";
+import TextControls from "./textcontrols";
+
+const Text = props => {
+  const [edit, setEdit] = React.useState(false);
+  const [textData, setTextData] = React.useState(props.textData);
+
+  const textRef = React.useRef();
+  const borderRef = React.useRef();
+
+  React.useEffect(() => {
+    const onMouseDown = e => {
+      if (e.target === borderRef.current) {
+        setTextData(s => handleMouseDown(s, e));
+      }
+    };
+    const onMouseMove = e => {
+      setTextData(s => handleMouseMove(s, e));
+    };
+    const onMouseUp = e => {
+      setTextData(s => handleMouseUp(s, e));
+    };
+
+    const onClick = e => {
+      setEdit(s => e.target.classList.contains(textData.id));
+    };
+
+    document.addEventListener("mousedown", onMouseDown);
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+    document.addEventListener("click", onClick);
+    return () => {
+      document.removeEventListener("click", onClick);
+      document.removeEventListener("mousedown", onMouseDown);
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+  }, [textData.id]);
+
+  return (
+    <div className="App">
+      <TextControls
+        onBoldClick={() => {
+          setTextData({
+            ...textData,
+            fontWeight: textData.fontWeight === "bold" ? "normal" : "bold"
+          });
+        }}
+        onItalicClick={() => {
+          setTextData({
+            ...textData,
+            fontStyle: textData.fontStyle === "italic" ? "normal" : "italic"
+          });
+        }}
+        onUnderlineClick={() => {
+          setTextData({
+            ...textData,
+            textDecoration:
+              textData.textDecoration === "underline" ? "none" : "underline"
+          });
+        }}
+        onFontSizeSelect={e => {
+          setTextData({
+            ...textData,
+            fontSize: parseInt(e.target.value)
+          });
+        }}
+        textData={textData}
+        edit={edit}
+      />
+      <Border edit={edit} ref={borderRef} textData={textData} color="lightgrey">
+        <BaseText
+          ref={textRef}
+          textData={textData}
+          edit={edit}
+          onClick={() => setEdit(true)}
+          onChange={e => setTextData({ ...textData, text: e.target.value })}
+        />
+      </Border>
+    </div>
+  );
+};
+
+export default Text;
